0afc71ee8e3a6c82ad45f11887adb28e5ebdf434
Lars <lars.wittenbrink@gmail.com>
2020-01-17 23:27:03 +0100

    Änderung Main

---
+++ src/com/willis_chico/vocabulary_trainer/FileEditor.java
@@ -0,0 +1,42 @@
+package com.willis_chico.vocabulary_trainer;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileEditor {
+    private final String path = "res/vocable.csv";
+    private File file;
+    private Scanner scanner;
+    private List<String[]> values;
+    private final String splitBy = ",";
+
+    public FileEditor() {
+        file = new File(path);
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        values = new ArrayList<>();
+    }
+
+    public void scan(){
+        String line;
+        String [] strings;
+        while(scanner.hasNextLine()){
+            line = scanner.nextLine();
+            strings = line.split(splitBy);
+            for (int i = 0; i < strings.length; i++) {
+                strings[i].replace("\"", "");
+                strings[i].trim();
+            }
+            values.add(strings);
+        }
+    }
+
+    public List<String[]> getValues() {
+        return values;
+    }
+}
--- src/com/willis_chico/vocabulary_trainer/Main.java
+++ src/com/willis_chico/vocabulary_trainer/Main.java
@@ -1,8 +1,16 @@
 package com.willis_chico.vocabulary_trainer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Raas");
+        FileEditor editor = new FileEditor();
+        List<String[]> values = editor.getValues();
+        for (int i = 0; i < values.size(); i++) {
+            System.out.println(values.get(i).toString());
+        }
     }
 }
