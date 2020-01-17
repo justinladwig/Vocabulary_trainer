6c5d0750017f2e34b760e80fe186ac49e3f6552e
BuildTools <unconfigured@null.spigotmc.org>
2020-01-17 20:34:11 +0100

    Initial commit

---
+++ src/com/willis_chico/vocabulary_trainer/GUI.java
@@ -0,0 +1,7 @@
+package com.willis_chico.vocabulary_trainer;
+
+import javax.swing.*;
+
+public class GUI {
+    private JPanel panel1;
+}
---
+++ src/com/willis_chico/vocabulary_trainer/Main.java
@@ -0,0 +1,8 @@
+package com.willis_chico.vocabulary_trainer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
