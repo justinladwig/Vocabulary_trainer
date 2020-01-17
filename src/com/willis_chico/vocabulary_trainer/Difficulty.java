7c890003d58af1a7475f9638cdbd2c2d8aa37dca
Lars <lars.wittenbrink@gmail.com>
2020-01-17 22:33:11 +0100

    Änderung Main

---
+++ src/com/willis_chico/vocabulary_trainer/Difficulty.java
@@ -0,0 +1,5 @@
+package com.willis_chico.vocabulary_trainer;
+
+public enum Difficulty {
+    HARD, NORMAL, EASY;
+}
--- src/com/willis_chico/vocabulary_trainer/vocabel.java
+++ src/com/willis_chico/vocabulary_trainer/vocabel.java
@@ -3,5 +3,35 @@ package com.willis_chico.vocabulary_trainer;
 public class vocabel {
     private String word;
     private String translation;
-    private String keineAhungunwasgenausfjfasdjfsdf;
+    private Difficulty difficulty;
+    private int numberWrong;
+    private int numberRight;
+
+    public vocabel(String word, String translation, Difficulty difficulty, int numberWrong, int numberRight) {
+        this.word = word;
+        this.translation = translation;
+        this.difficulty = difficulty;
+        this.numberWrong = numberWrong;
+        this.numberRight = numberRight;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public int getNumberWrong() {
+        return numberWrong;
+    }
+
+    public int getNumberRight() {
+        return numberRight;
+    }
 }
