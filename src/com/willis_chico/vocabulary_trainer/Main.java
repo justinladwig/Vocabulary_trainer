e75b081aa2a5a53ae8587041acaa7f4e7f237daf
Lars <lars.wittenbrink@gmail.com>
2020-01-17 21:16:48 +0100

    Änderung Main

--- src/com/willis_chico/vocabulary_trainer/Main.java
+++ src/com/willis_chico/vocabulary_trainer/Main.java
@@ -3,6 +3,6 @@ package com.willis_chico.vocabulary_trainer;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Github Test");
+        System.out.println("Github Test für Justin");
     }
 }
