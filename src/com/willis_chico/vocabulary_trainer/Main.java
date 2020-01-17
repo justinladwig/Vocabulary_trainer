2ec45f579ad706ca473baa76c6aec76800217def
Mischa <timtrodat2@gmail.com>
2020-01-17 22:04:48 +0100

    change "Hello Raas" message

--- src/com/willis_chico/vocabulary_trainer/Main.java
+++ src/com/willis_chico/vocabulary_trainer/Main.java
@@ -3,6 +3,6 @@ package com.willis_chico.vocabulary_trainer;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Github Test für Justin");
+        System.out.println("Hello Raas");
     }
 }
