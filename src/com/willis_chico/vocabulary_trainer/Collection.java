7e6aa1ce686d8bc323125520a80debde481b1a75
Justin Ladwig <justin.ladwig.2003@gmail.com>
2020-01-18 19:52:53 +0100

    Add and begin Collection.java

---
+++ src/com/willis_chico/vocabulary_trainer/Collection.java
@@ -0,0 +1,27 @@
+package com.willis_chico.vocabulary_trainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection {
+    List<vocabel> vocabulary;
+    FileEditor editor;
+
+    public Collection() {
+        vocabulary = new ArrayList<>();
+        editor = new FileEditor();
+    }
+
+    public void importVocabulary(){
+        List<String[]> values;
+        editor.scan();
+        values = editor.getValues();
+    }
+    public void exportVocabulary(){
+        List<String[]> values;
+
+
+        //editor.setValues(values);
+        editor.write();
+    }
+}
