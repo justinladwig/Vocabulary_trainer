cf959ac1d1d079b1137941327f53ecd4337273d4
Justin Ladwig <justin.ladwig.2003@gmail.com>
2020-01-19 22:03:17 +0100

    Add infinity loop to GUI Main method.

--- src/com/willis_chico/vocabulary_trainer/GUI.java
+++ src/com/willis_chico/vocabulary_trainer/GUI.java
@@ -1,4 +1,29 @@
 package com.willis_chico.vocabulary_trainer;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUI {
+    private JPanel panel1;
+    private JButton button1;
+
+    public GUI() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "Hallo Lars");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            JFrame frame = new JFrame("Vocable Trainer");
+            frame.setContentPane(new GUI().panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
+   }
 }
