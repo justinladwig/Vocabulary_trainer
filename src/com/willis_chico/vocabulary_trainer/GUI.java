80f0890ae58b2a2342b8d7b6275ac03f73f44468
Lars <lars.wittenbrink@gmail.com>
2020-01-20 20:42:18 +0100

    GUI

--- src/com/willis_chico/vocabulary_trainer/GUI.java
+++ src/com/willis_chico/vocabulary_trainer/GUI.java
@@ -1,8 +1,40 @@
 package com.willis_chico.vocabulary_trainer;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
-    private JButton button1;
+    private JButton lernenButton;
     private JPanel panel1;
+    private JButton suchenButton;
+    private JButton vokabelHinzufügenButton;
+    private JButton sortierenButton;
+
+    public GUI() {
+        lernenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        suchenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        vokabelHinzufügenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        sortierenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
 }
